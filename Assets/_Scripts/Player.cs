18cb014833707cf0fb9a477deac451d815518b96
Guillem Pujol Griñó <guille8.bcn@gmail.com>
2019-11-01 10:15:00 +0100

    First commit

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Variables
+    public float maxHealth, maxThirst, maxHunger, maxOxigen;
+    public float thirstIncreaseRate, hungerIncreaseRate, oxigenDecreaseRate;
+    public float health, thirst, hunger, oxigen;
+
+    private bool playerDead;
+
+    // Functions
+    public  void Start()
+    {
+        health = maxHealth;
+        oxigen = maxOxigen;
+    }
+
+    public void Update()
+    {
+        // thirst and  hunger increase
+        if(!playerDead)
+        {
+            thirst += thirstIncreaseRate * Time.deltaTime;
+            hunger += hungerIncreaseRate * Time.deltaTime;
+            oxigen -= oxigenDecreaseRate * Time.deltaTime;
+        }
+
+        if (thirst >= maxThirst)
+            Die();
+        if (hunger >= maxHunger)
+            Die();
+        if (oxigen <= 0)
+            Die();
+
+    }
+
+
+    public  void Die()
+    {
+        playerDead = true;
+        
+    }
+
+
+
+
+}
