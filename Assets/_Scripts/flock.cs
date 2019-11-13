88b76b3ec8ff0cb4151bb09cacaae909eb3e6bf4
Elisa <38461451+elisa-mamolo@users.noreply.github.com>
2019-11-13 14:33:28 +0100

    added comments and fixed error with range

--- Assets/_Scripts/flock.cs
+++ Assets/_Scripts/flock.cs
@@ -17,14 +17,14 @@ public class flock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = Random.Range(0.5f, 1);
+        speed = Random.Range(0.5f,1);
     }
 
     // Update is called once per frame
     void Update()
     {
         //run the apply rules once every 5 times randomly
-        if (Random.Range(0.5) < 1)
+        if(Random.Range(0,5) < 1)
             ApplyRules();
         //fish is going to swim forward  
         transform.Translate(0, 0, Time.deltaTime * speed);
@@ -51,26 +51,40 @@ public class flock : MonoBehaviour
             if (go != this.gameObject)
             {
                 dist = Vector3.Distance(go.transform.position, this.transform.position);
+                //if distance is 2 the fish is part of a group
                 if (dist <= neighbourDistance)
                 {
+                    //add center and group size
                     vcentre += go.transform.position;
                     groupSize++;
+                    //if less than 1 oway from the group
                     if(dist < 1.0f)
                     {
+                        //we will go in another direction
                         vavoid = vavoid + (this.transform.position - go.transform.position);
                     }
+
+                    //calculate speed from flock script attached to neighbour fish and have a average speed for the group at line 74
                     flock anotherFlock = go.GetComponent<flock>();
                     gSpeed = gSpeed + anotherFlock.speed;
                 }
             }
         }
-
+        //if we are in agroup
         if(groupSize > 0)
         {
+            //calc avg center and speed of group
             vcentre = vcentre / groupSize + (goalPos - this.transform.position);
             speed = gSpeed / groupSize;
 
             Vector3 direction = (vcentre + vavoid) - transform.position;
+            //if direction is not = to 0 we will change direction
+            if (direction != Vector3.zero)
+                //slowly turn from one rotation to another
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                        Quaternion.LookRotation(direction),
+                                                        rotationSpeed * Time.deltaTime);
+
         }
     }
 }
